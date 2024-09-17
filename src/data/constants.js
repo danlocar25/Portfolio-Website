1d12332755e2e69b761e5762fff89f3e16c0ff56
Dan <92004865+danlocar25@users.noreply.github.com>
2024-09-17 23:34:53 +0800

    Revise/Added some informations.

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,10 +4,10 @@ export const Bio = {
     "Junior Software Developer",
   ],
   description:
-    "I am a dedicated junior IT professional with a recent degree in Information Technology. I am eager to begin my career and make meaningful contributions to the tech industry.",
+    "I am a dedicated Junior IT professional with a recent bachelor's degree in Information Technology. I am eager to begin my career and make meaningful contributions to the tech industry.",
   github: "https://github.com/danlocar25",
   resume:
-    "https://drive.google.com/file/d/17ihkV3KSkCuTPD4zxbz-hJZjw-Q78nAo/view?usp=sharing",
+    "https://drive.google.com/file/d/1gz4KCZnY8590WvDWp-t8YOPI3dS8u48x/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/dan-carlo-magpantay/",
   insta: "https://www.instagram.com/dan.olrac/",
   email: "dancarlom03@gmail.com",
@@ -53,14 +53,23 @@ export const skills = [
           "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
       },
       {
-        name: "React",
+        name: "Tailwind",
         image:
-          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+          "https://static-00.iconduck.com/assets.00/tailwind-css-icon-512x307-1v56l8ed.png",
       },
       {
         name: "jQuery",
         image:
           "https://static-00.iconduck.com/assets.00/jquery-icon-1024x1024-rar4nqfl.png",
+      },
+      {
+        name: "React",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Express",
+        image: "https://symbols.getvecta.com/stencil_25/63_nodejs.124d3fe0e1.png",
       },
       {
         name: "ASP.Net Core",
@@ -98,7 +107,7 @@ export const skills = [
     ],
   },
   {
-    title: "Others",
+    title: "Tools",
     skills: [
       {
         name: "Git",
@@ -123,23 +132,63 @@ export const skills = [
       {
         name: "Android Studio",
         image:
-          "https://cdn-icons-png.flaticon.com/256/5436/5436946.png",
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/android-studio-icon.png",
       },
       {
-        name: "Adobe XD",
+        name: "Adobe Illustrator",
         image:
-          "https://www.logo.wine/a/logo/Adobe_XD/Adobe_XD-Logo.wine.svg",
+          "https://cdn-icons-png.flaticon.com/256/5436/5436946.png",
       },
       {
         name: "Figma",
         image:
           "https://cdn4.iconfinder.com/data/icons/logos-brands-in-colors/3000/figma-logo-512.png",
       },
+      {
+        name: "TensorFlow Lite",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-955x1024-hd4xzbqj.png",
+      },
     ],
   },
 ];
 
 export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVK-Rc_T_JF2kmR5fytwC9HpjYzC9nigCms8OgcxwPg&s",
+    role: "IT Quizbee 1st Runner Up",
+    company: "College of Sciences, Technology and Communications Inc.",
+    date: "February 2023",
+    desc: "I secured the 1st place in the IT QuizBee, a competition participated by BSIT students from 1st to 4th year. The event showcased the collective knowledge and expertise of participants across various levels of the BSIT curriculum, providing a platform for students to test their skills and understanding of IT concepts.",
+
+    doc: require("../images/quizbee.jpg"),
+  },
+  {
+    id: 0,
+    img: "https://cdn-1.webcatalog.io/catalog/great-learning/great-learning-icon-filled-256.png?v=1709633110716",
+    role: "Introduction to Cyber Security",
+    company: "Great Learning Academy",
+    date: "March 2023",
+    desc: "This online course from Great Learning Academy provided a thorough understanding about Cybersecurity and types of cybercrimes. I also learned about malware and the risks that can be encountered in cyberspace and the ways to avoid it.",
+    skills: [
+      "Cybersecurity",
+    ],
+    doc: require("../images/cyber.png"),
+  },
+  {
+    id: 0,
+    img: "https://www.freecodecamp.org/news/content/images/2021/05/freecodecamp-org-gravatar.jpeg",
+    role: "Machine Learning with Python",
+    company: "freeCodeCamp",
+    date: "August 2023",
+    desc: "This online course from freeCodeCamp helped me to understand machine learning using the Python programming language. I learned to use several machine learning algorithms such as KNN, Linear Regression and Neural Networks.",
+    skills: [
+      "Machine Learning Algorithm",
+      "Python",
+    ],
+    doc: require("../images/ml.png"),
+  },
   {
     id: 0,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVK-Rc_T_JF2kmR5fytwC9HpjYzC9nigCms8OgcxwPg&s",
@@ -153,16 +202,6 @@ export const experiences = [
   {
     id: 0,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVK-Rc_T_JF2kmR5fytwC9HpjYzC9nigCms8OgcxwPg&s",
-    role: "IT Quizbee 1st Runner Up",
-    company: "College of Sciences, Technology and Communications Inc.",
-    date: "August 2023",
-    desc: "I secured the 1st place in the IT QuizBee, a competition participated by BSIT students from 1st to 4th year. The event showcased the collective knowledge and expertise of participants across various levels of the BSIT curriculum, providing a platform for students to test their skills and understanding of IT concepts.",
-
-    doc: require("../images/quizbee.jpg"),
-  },
-  {
-    id: 0,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiVK-Rc_T_JF2kmR5fytwC9HpjYzC9nigCms8OgcxwPg&s",
     role: "Hackathon Competition Champion",
     company: "College of Sciences, Technology and Communications Inc.",
     date: "February 2024",
@@ -172,22 +211,37 @@ export const experiences = [
   },
   {
     id: 0,
-    img: "https://www.freecodecamp.org/news/content/images/2021/05/freecodecamp-org-gravatar.jpeg",
-    role: "JavaScript Algortihms and Data Structures",
-    company: "freeCodeCamp",
-    date: "April 2024",
-    desc: "This is an online course from freeCodeCamp. I learned the fundamentals of JavaScript, including algorithms and data structures, essential for web development.",
+    img: "https://static.vecteezy.com/system/resources/thumbnails/020/190/709/small_2x/microsoft-logo-microsoft-icon-free-free-vector.jpg",
+    role: "Career Essentials in Software Development by Microsoft and LinkedIn",
+    company: "Microsoft",
+    date: "February 2024",
+    desc: "This is an online course from Microsoft and LinkedIn Learning. Here, I learned about the lifecyle involved in software development, starting from planning, analysis, design, development, testing, deployment, and up to maintenance.",
     skills: [
-      "JavaScript",
+      "Software Development",
+      "SDLC",
+      "Programming",
     ],
-    doc: require("../images/Certificate.png"),
+    doc: require("../images/microsoft.png"),
+  },
+  {
+    id: 0,
+    img: "https://cdn-1.webcatalog.io/catalog/great-learning/great-learning-icon-filled-256.png?v=1709633110716",
+    role: "Network Security",
+    company: "Great Learning Academy",
+    date: "February 2024",
+    desc: "This online course from Great Learning Academy help me to understand the concepts and types of Cybersecurity and Network Security. The course covered principles, methodologies, and best practices of cybersecurity, alongside in-depth insights into Network Security.",
+    skills: [
+      "Network Security",
+      "Cybersecurity",
+    ],
+    doc: require("../images/net.png"),
   },
   {
     id: 0,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScIEE9BdLqCBMVavuF5E41TXjBVmJa9-K_pY-XpIxRBw&s",
     role: "DICT-ICT018 Basic Level of Software Engineering",
     company: "Department of Information and Communications Technology",
-    date: "March 2024",
+    date: "February 2024",
     desc: "This is an online course offered by the Department of Information and Communications Technology of the Philippines. I feel fortunate to have earned this certificate because it has limited access, with numerous courses available exclusively to government employees and students. The course provided me with fundamental knowledge and principles of Software Engineering, including the Software Development Lifecycle (SDLC).",
     skills: [
       "Software Engineering",
@@ -225,55 +279,15 @@ export const experiences = [
   },
   {
     id: 0,
-    img: "https://static.vecteezy.com/system/resources/thumbnails/020/190/709/small_2x/microsoft-logo-microsoft-icon-free-free-vector.jpg",
-    role: "Career Essentials in Software Development by Microsoft and LinkedIn",
-    company: "Microsoft",
-    date: "February 2024",
-    desc: "This is an online course from Microsoft and LinkedIn Learning. Here, I learned about the lifecyle involved in software development, starting from planning, analysis, design, development, testing, deployment, and up to maintenance.",
-    skills: [
-      "Software Development",
-      "SDLC",
-      "Programming",
-    ],
-    doc: require("../images/microsoft.png"),
-  },
-  {
-    id: 0,
     img: "https://www.freecodecamp.org/news/content/images/2021/05/freecodecamp-org-gravatar.jpeg",
-    role: "Machine Learning with Python",
+    role: "JavaScript Algorithms and Data Structures",
     company: "freeCodeCamp",
-    date: "August 2024",
-    desc: "This online course from freeCodeCamp helped me to understand machine learning using the Python programming language. I learned to use several machine learning algorithms such as KNN, Linear Regression and Neural Networks.",
+    date: "April 2024",
+    desc: "This is an online course from freeCodeCamp. I learned the fundamentals of JavaScript, including algorithms and data structures, essential for web development.",
     skills: [
-      "Machine Learning Algorithm",
-      "Python",
+      "JavaScript",
     ],
-    doc: require("../images/ml.png"),
-  },
-  {
-    id: 0,
-    img: "https://cdn-1.webcatalog.io/catalog/great-learning/great-learning-icon-filled-256.png?v=1709633110716",
-    role: "Network Security",
-    company: "Great Learning Academy",
-    date: "February 2024",
-    desc: "This online course from Great Learning Academy help me to understand the concepts and types of Cybersecurity and Network Security. The course covered principles, methodologies, and best practices of cybersecurity, alongside in-depth insights into Network Security.",
-    skills: [
-      "Network Security",
-      "Cybersecurity",
-    ],
-    doc: require("../images/net.png"),
-  },
-  {
-    id: 0,
-    img: "https://cdn-1.webcatalog.io/catalog/great-learning/great-learning-icon-filled-256.png?v=1709633110716",
-    role: "Introduction to Cyber Security",
-    company: "Great Learning Academy",
-    date: "March 2023",
-    desc: "This online course from Great Learning Academy provided a thorough understanding about Cybersecurity and types of cybercrimes. I also learned about malware and the risks that can be encountered in cyberspace and the ways to avoid it.",
-    skills: [
-      "Cybersecurity",
-    ],
-    doc: require("../images/cyber.png"),
+    doc: require("../images/Certificate.png"),
   },
 ];
 
@@ -283,9 +297,18 @@ export const education = [
     img: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADgAAAA4CAMAAACfWMssAAAAn1BMVEX///8AAADTQ1DNFy16enrp6enIAAD99vf19fWfn59ycnLMDyh1dXXU1NT6+vq4uLjLABzLABfi4uKFhYXv7+8uLi5TU1PMzMyxsbFqamrDw8Pa2trKAA7MBiT78PHRNUP1293lnqPacHj44+Xxzc8mJibrs7jopqxHR0eoqKgYGBjef4buvsHvxcjZY27PKTuRkZHki5HUTlg7OzteXl6xURa6AAABh0lEQVRIie2SWZOCMBCEpw2nSBQEUQ7xQoH1dv//b9uJPqyLWrVW7cM+8FUFimSaznRC1NLS0vJvsHx+THMxe1vZ5zFPB4u3hUFFtPSM1csCuwC6zxbUpDR3eedKvpabjXm3rKGsathPhENdPT+8zHGcLDNmq+1u973qA1yCIUnSA/UdcbllkRtQFZnL5XTfyR0hDp0Dmev1neMRGtEEPaAEYusC4OgCIeCP45NhnDmhgfB21KQEpxfiE6HUMBzBDVDbmFAB13bPzmBOdHaEMX0QAjpZwIi77EJDHY1xmSDmeZro2dVqIIR40CWqxRhliIRNNBSXcnQsECS4ULlMldUsFdn+QRhzAVtxi6Sj1jCmJHJRk41hYm8950C09oTacIMIcMeADVjcogabU664Rd5tj06ZslpkIn1yC0YcY6h3OR2Ukn+DwtdQcYtxn3LDYCuRpob5KCQ90VVErlTHLf3bKfLgK2GaSmDeXk/h/cnGVP9V7Q8qFdEPEutXQimbhi0tLS1/zBcsLhkDVYhRKAAAAABJRU5ErkJggg==",
     school: "Software Developer Intern",
     date: "April 2024 - August 2024",
-    grade: "Python/Django, jQuery, PostgreSQL, RESTful API, Git/GitLab, Redmine",
+    grade: "Bootstrap | jQuery | Django | PostgreSQL | RESTful API | Redmine | Git",
     desc: "The project we created during our internship was a web application designed to automate hotfix processes, including generating release notes for Open iT's products and software.",
     degree: "Open iT • Internship",
+  },  
+  {
+    id: 0,
+    img: "https://scontent.fpag2-1.fna.fbcdn.net/v/t39.30808-1/460445581_2595303437524587_8454155183776776769_n.jpg?stp=dst-jpg_s200x200&_nc_cat=106&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=0MadhXUE_IQQ7kNvgHGP9VD&_nc_ht=scontent.fpag2-1.fna&_nc_gid=ACCsfja4On2sfoGrJv0_1K9&oh=00_AYCZlbGMho0NsbJ9ZF2CnvsrhF2Mwkie52XwIaFh2_R4lA&oe=66EF6250",
+    school: "Freelance Software Developer",
+    date: "August 2024 - Present",
+    grade: "Tailwind | React | Express | NodeJS | MongoDB | Git",
+    desc: "I am a Co-Founder and CEO of BuilDevs, a startup software solutions company specializing in offering and delivering software projects, particularly for IT and Computer Science capstone projects from colleges and universities. BuilDevs consists of four dedicated members working together to provide innovative and tailored solutions to meet the unique needs of our clients. We operate on a freelance basis and are not engaged in this endeavor full-time yet.",
+    degree: "BuilDevs",
   },  
 ];
 
@@ -295,7 +318,7 @@ export const projects = [
     title: "ThriftShop",
     date: "April 2022",
     description:
-      "ThriftShop is an e-commerce website whose products are thrifted outfits such as shirts, jeans and shoes. This is my school project when I was a sophomore as an IT student in our Web Development subject.",
+      "ThriftShop is an e-commerce static website that focuses on selling thrifted outfits such as shirts, jeans, and shoes. This project was developed as a school assignment during my sophomore year as an IT student in the Web Development subject.",
     image: 
       "https://github.com/danlocar25/ThriftShop/blob/main/images/thriftshop.png?raw=true",
     tags: [
@@ -342,7 +365,7 @@ export const projects = [
     description: "This is our Capstone Project titled 'An Android-based Mobile Application for Arabica Coffee Leaf Disease Detection through Digital Image Processing using Convolutional Neural Network Algorithm.' It features the ability to identify diseases on coffee leaves and determine the severity level of the detected diseases.",
     image:
       "https://github.com/nickolandermilan30/Coffeetect/blob/master/app/src/main/res/drawable/pview.png?raw=true",
-    tags: ["Java", "Python", "Android Studio", "TensorFlow Lite", "Google Colab", "Firebase"],
+    tags: ["Android Studio", "Java", "Python", "TensorFlow Lite", "Google Colab", "Firebase"],
     category: "android app",
     github: "https://github.com/nickolandermilan30/Coffeetect",
     webapp: "https://drive.google.com/drive/folders/1IlIMXH0TcEj4grC__9--hI2Ry69m7z_T?usp=sharing",
